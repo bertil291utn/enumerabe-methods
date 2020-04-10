77860ec5a16b05e5d8e01f92517d670eaefb25ee
bertil291utn <bertilt4@gmail.com>
2020-04-09 23:23:23 -0500

    My select, all,any, none and count methods - Clone these methods from ruby method

--- enumerable_method_asignment.rb
+++ enumerable_method_asignment.rb
@@ -18,6 +18,49 @@ module Enumerable
       index += 1
     end
   end
+
+  def my_select
+    var_seleccionado = []
+    my_each { |elem| var_seleccionado.push(elem) if yield(elem) }
+    var_seleccionado
+  end
+
+  def my_all?
+    if !block_given?
+      my_each { |elem| return false if elem.nil? || !elem }
+    else
+      my_each { |elem| return false unless yield(elem) }
+    end
+    true
+  end
+
+  def my_any?
+    if !block_given?
+      my_each { |elem| return true unless elem.nil? || !elem }
+    else
+      my_each { |elem| return true if yield(elem) }
+    end
+    false
+  end
+
+  def my_none?
+    my_each { |elem| return false if yield(elem) }
+    true
+  end
+
+  def my_count(arguments = nil)
+    return length if !block_given? && arguments.nil?
+
+    count = 0
+    my_each do |elem|
+      if arguments
+        count += 1 if elem == arguments
+      elsif yield(elem)
+        count += 1
+      end
+    end
+    count
+  end
 end
 
 # end module
