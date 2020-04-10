d911e2b2a58e4cce65b99145fc3fd3eea2f5ba05
bertil291utn <bertilt4@gmail.com>
2020-04-10 13:16:27 -0500

    FIxing first code review - Add return enumerator in my_each,my_each_with_index,my_select and my_map methods -  Add to check when enters a integer, module and a regexp in my_all?, my_any? and my_none? methods - Accept symbol and procedures as argument, also a range as enter data my_inject method -  Disabling rubocop cops

--- enumerable_method_asignment.rb
+++ enumerable_method_asignment.rb
@@ -1,8 +1,12 @@
-# rubocop:disable Style/CaseEquality
+# rubocop:disable Metrics/ModuleLength
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
 
 # method start
 module Enumerable
   def my_each
+    return to_enum :my_each unless block_given?
+
     index = 0
     while index < size
       yield(self[index])
@@ -11,6 +15,8 @@ module Enumerable
   end
 
   def my_each_with_index
+    return to_enum :my_each unless block_given?
+
     index = 0
     while index < size
       elem = self[index]
@@ -20,31 +26,55 @@ module Enumerable
   end
 
   def my_select
+    return to_enum :my_each unless block_given?
+
     var_seleccionado = []
     my_each { |elem| var_seleccionado.push(elem) if yield(elem) }
     var_seleccionado
   end
 
-  def my_all?
-    if !block_given?
+  def my_all?(argument = nil)
+    if argument.nil? && !block_given?
       my_each { |elem| return false if elem.nil? || !elem }
-    else
+    elsif argument.nil?
       my_each { |elem| return false unless yield(elem) }
+    elsif argument.is_a? Regexp
+      my_each { |elem| return false unless elem.match(argument) }
+    elsif argument.is_a? Module
+      my_each { |elem| return false unless elem.is_a?(argument) }
+    else
+      my_each { |elem| return false unless elem == argument }
     end
     true
   end
 
-  def my_any?
-    if !block_given?
+  def my_any?(argument = nil)
+    if argument.nil? && !block_given?
       my_each { |elem| return true unless elem.nil? || !elem }
-    else
+    elsif argument.nil?
       my_each { |elem| return true if yield(elem) }
+    elsif argument.is_a? Regexp
+      my_each { |elem| return true if elem.match(argument) }
+    elsif argument.is_a? Module
+      my_each { |elem| return true if elem.is_a?(argument) }
+    else
+      my_each { |elem| return true if elem == argument }
     end
     false
   end
 
-  def my_none?
-    my_each { |elem| return false if yield(elem) }
+  def my_none?(argument = nil)
+    if argument.nil? && !block_given?
+      my_each { |elem| return false if elem }
+    elsif argument.nil?
+      my_each { |elem| return false if yield(elem) }
+    elsif argument.is_a? Regexp
+      my_each { |elem| return false if elem.match(argument) }
+    elsif argument.is_a? Module
+      my_each { |elem| return false if elem.is_a?(argument) }
+    else
+      my_each { |elem| return false if elem == argument }
+    end
     true
   end
 
@@ -63,6 +93,8 @@ module Enumerable
   end
 
   def my_map(procedure = nil)
+    return to_enum :my_map unless block_given?
+
     returned_array = []
     my_each do |elem|
       returned_array.push((procedure ? procedure.call(elem) : yield(elem)))
@@ -70,25 +102,31 @@ module Enumerable
     returned_array
   end
 
-  def my_inject(inicio = nil)
-    if inicio.nil?
-      resultado = self[0]
-      index = 1
+  def my_inject(*argument)
+    array = is_a?(Array) ? self : to_a
+    inicio = argument[0] if argument[0].is_a?(Integer)
+    es_simbolo = argument[0] if argument[0].is_a?(Symbol)
+    resultado = inicio
+    if es_simbolo
+      array.my_each do |elem|
+        resultado = resultado ? resultado.send(es_simbolo, elem) : elem
+      end
     else
-      resultado = inicio
-      index = 0
+      array.my_each do |elem|
+        resultado = resultado ? yield(resultado, elem) : elem
+      end
     end
-
-    self[index..-1].my_each { |elem| resultado = yield(resultado, elem) }
     resultado
   end
 end
 
 # end module
 
-def multiply_els(arr, start = nil)
-  total = arr.my_inject(start) { |elem, siguiente| elem * siguiente }
+def multiply_els(arr, num_inicial = nil)
+  total = arr.my_inject(num_inicial) { |elem, siguiente| elem * siguiente }
   total
 end
 
-# rubocop:disable Style/CaseEquality
+# rubocop:enable Metrics/ModuleLength
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
