c54e0d5d6513d6a1f75e3c49c70d1788a2e3c75f
bertil291utn <bertilt4@gmail.com>
2020-04-17 17:56:42 -0500

    My each with index method - Add block and block no given cases - Add no given method to my_each method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -21,5 +21,58 @@ describe Enumerable do
         expect(array_string).to eql(result)
       end
     end
+
+    context 'no blocken given' do
+      it 'is returning an enumerator value' do
+        result = array_string.my_each
+        expect(result).to be_an Enumerator
+      end
+    end
+  end
+
+  describe 'my_each_with_index method' do
+    context 'numbers array ' do
+      it 'is returning same array as original numbers one' do
+        my_result = []
+        result = []
+        array_numbers.my_each_with_index { |elem, i| my_result.push(elem + i) }
+        array_numbers.each_with_index { |elem, i| result.push(elem + i) }
+        expect(my_result).to eql(result)
+      end
+    end
+
+    context 'string array' do
+      it 'is returning same array as original string one' do
+        my_result = []
+        result = []
+        array_string.my_each_with_index { |elem, i| my_result.push(elem + i.to_s) }
+        array_string.each_with_index { |elem, i| result.push(elem + i.to_s) }
+        expect(my_result).to eql(result)
+      end
+    end
+
+    context 'no blocken given' do
+      it 'is returning an enumerator value' do
+        result = array_string.my_each_with_index
+        expect(result).to be_an Enumerator
+      end
+    end
+  end
+
+  describe 'my_select method' do
+    context 'no blocken given' do
+      it 'is returning an enumerator value' do
+        result = array_string.my_select
+        expect(result).to be_an Enumerator
+      end
+    end
+    # check this
+    context 'numbers array ' do
+      it 'is returning same array as original numbers one' do
+        result = []
+        array_numbers.my_select { |elem| result.push(elem) }
+        expect(array_numbers).to eql(result)
+      end
+    end
   end
 end
