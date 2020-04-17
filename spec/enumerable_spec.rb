815dbf939a40ab573ed05bb29bfc850348a0f3ad
bertil291utn <bertilt4@gmail.com>
2020-04-17 17:41:14 -0500

    My each method - Add two tests to my_each method array (string and numbers)

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,12 +1,25 @@
 require './enumerable_method_asignment'
 
 describe Enumerable do
-  context 'my_each method' do
-    let(:array_var) { [10, 2, 3, 4, 1, 5] }
-    it 'is returning same array as original one' do
-      result = []
-      array_var.my_each { |elem| result.push(elem) }
-      expect(array_var).to eql(result)
+  let(:array_numbers) { [10, 2, 3, 4, 1, 5] }
+  let(:array_string) { %w[10 2 3 4 1 5] }
+  let(:range) { (1..20) }
+
+  describe 'my_each method' do
+    context 'numbers array ' do
+      it 'is returning same array as original number one' do
+        result = []
+        array_numbers.my_each { |elem| result.push(elem) }
+        expect(array_numbers).to eql(result)
+      end
+    end
+
+    context 'string array' do
+      it 'is returning same array as original string one' do
+        result = []
+        array_string.my_each { |elem| result.push(elem) }
+        expect(array_string).to eql(result)
+      end
     end
   end
 end
