c38f6d367a2ccebeda71dd36aaba3fde84fd5f1c
bertil291utn <bertilt4@gmail.com>
2020-04-18 16:10:05 -0500

    Add  `describe` block every method - Add an mix array whit nil and false values

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,32 +1,33 @@
 require './enumerable_method_asignment'
 
-describe Enumerable do
+RSpec.describe Enumerable do
   let(:array_numbers) { [10, 2, 3, 4, 1, 5] }
   let(:array_string) { %w[10 2 3 4 1 5] }
+  let(:array_mix) { [nil, true, 99] }
   let(:range) { (1..20) }
 
-  describe 'my_each method' do
-    context 'numbers array ' do
-      it 'is returning same array as original number one' do
-        result = []
-        array_numbers.my_each { |elem| result.push(elem) }
-        expect(array_numbers).to eql(result)
-      end
+  describe '#my_each' do
+    it 'is returning same array as original number one' do
+      result = []
+      array_numbers.my_each { |elem| result.push(elem) }
+      expect(array_numbers).to eql(result)
     end
 
-    context 'string array' do
-      it 'is returning same array as original string one' do
-        result = []
-        array_string.my_each { |elem| result.push(elem) }
-        expect(array_string).to eql(result)
-      end
+    it 'is returning same array as original string one' do
+      result = []
+      array_string.my_each { |elem| result.push(elem) }
+      expect(array_string).to eql(result)
     end
 
-    context 'no blocken given' do
-      it 'is returning an enumerator value' do
-        result = array_string.my_each
-        expect(result).to be_an Enumerator
-      end
+    it 'is returning an enumerator value when there\'s no blocken given' do
+      result = array_string.my_each
+      expect(result).to be_an Enumerator
+    end
+
+    it 'is returning same array as original mix array' do
+      result = []
+      array_mix.my_each { |elem| result.push(elem) }
+      expect(array_mix).to eql(result)
     end
   end
 
